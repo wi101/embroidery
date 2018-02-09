86ccb24291a2b5c5456d98797873f01edf03b8a0
Wiem Zine El Abidine <wiemzin@gmail.com>
2018-02-10 00:02:32 +0100

    implemented preferedSize in logos

--- src/main/scala/embroidery/asciiArt/logos/LogoArt.scala
+++ src/main/scala/embroidery/asciiArt/logos/LogoArt.scala
@@ -20,13 +20,32 @@ case class LogoArt(url: URL, art: Art) extends Embroidery {
   override def drawImage(): BufferedImage = {
     val emptyImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB)
     readLogo().fold(emptyImage){ logo =>
-      val (width, height) = (70, 20) //FIXME calculate the prefered height & width
-    val tmp = logo.image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+      val (width, height) = calculatePreferedSize(logo.image.getWidth, logo.image.getHeight)
+      val tmp = logo.image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
       val scaledImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
       val g = scaledImg.createGraphics()
       g.drawImage(tmp, 0, 0, width, height, null)
       g.dispose()
       scaledImg
     }
+  }
+
+  def calculatePreferedSize(width: Int, height: Int): (Int, Int) = {
+    assert(width > 0 && height > 0)
+
+    val (maxW, maxH) = (40, 30)
+    val (restW, restH) = (width % maxW, height % maxH)
+    val newW = if (width == restW) width else maxW
+    val newH = if (height == restH) height else maxH
+
+    val (calculatedW, calculatedH) = if (width > height || width / height == 1) {
+      val q = width / height
+      (newW, newW / q)
+    }
+    else if (height > width || height / width == 1) {
+      val q = height / width
+      (newH / q, newH)
+    } else (newH, newW)
+    (calculatedW * 2, calculatedH)
   }
 }
--- src/test/scala/embroidery/asciiArt/logos/Example.scala
+++ src/test/scala/embroidery/asciiArt/logos/Example.scala
@@ -2,5 +2,4 @@ package embroidery.asciiArt.logos
 
 object Example extends App {
   printWithArt("src/test/scala/embroidery/asciiArt/logos/images/logo.jpg")
-
 }
