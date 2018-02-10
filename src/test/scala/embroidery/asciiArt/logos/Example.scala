e8fbb1e98778ba322d704b3f49c93d4231630575
Wiem Zine El Abidine <wiemzin@gmail.com>
2018-02-10 02:18:35 +0100

    organized images

--- src/test/scala/embroidery/asciiArt/logos/Example.scala
+++ src/test/scala/embroidery/asciiArt/logos/Example.scala
@@ -6,7 +6,7 @@ object Example extends App {
   printWithArt("src/test/scala/embroidery/asciiArt/logos/images/cat.png") //225 * 225
   printWithArt("src/test/scala/embroidery/asciiArt/logos/images/smiley.png") //720 * 720
 
-  printWithArt("src/test/scala/embroidery/asciiArt/logos/images/logo.jpg") //800 * 237
+  printWithArt("src/test/scala/embroidery/asciiArt/logos/images/scala.jpg") //800 * 237
   printWithArt("src/test/scala/embroidery/asciiArt/logos/images/play.png") //511 * 231
   printWithArt("src/test/scala/embroidery/asciiArt/logos/images/akka.png") //350 * 144
   printWithArt("src/test/scala/embroidery/asciiArt/logos/images/fleur.jpg") //500 * 383
